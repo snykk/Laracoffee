8dfc8ae4c5add79bb6cbdf50eb6d00fd1061eee7
Najib Fikri <najibfikri13@gmail.com>
2022-07-21 10:48:16 +0700

    create landing page

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,17 @@
+@extends('/templates/main')
+
+@section("content")
+<header class="masthead">
+    <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+        <div class="d-flex justify-content-center">
+            <div class="text-center">
+                <h1 class="mx-auto my-0 text-uppercase">Laracoffee</h1>
+                <h2 class="text-white-50 mx-auto mt-2 mb-5">
+                    New way to enjoy quality coffee
+                </h2>
+                <a class="btn btn-primary" href="auth">Get Started</a>
+            </div>
+        </div>
+    </div>
+</header>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/landing.index', [
+        "title" => "Landing Page",
+        "css" => "landing"
+    ]);
 });
