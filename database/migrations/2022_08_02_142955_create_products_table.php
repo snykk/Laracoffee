871483e374b4d6ce7b6950a187ff4007c166a016
Najib Fikri <najibfikri13@gmail.com>
2022-08-02 22:17:57 +0700

    prepare product data

---
+++ database/migrations/2022_08_02_142955_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 15)->unique();
+            $table->text('orientation');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->double('discount', 3, 2);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Str;
@@ -61,5 +63,17 @@ class DatabaseSeeder extends Seeder
             "id" => 2,
             "role_name" => "Customer"
         ]);
+
+        for ($i = 1; $i <= 6; $i++) {
+            Product::create([
+                "product_name" => "Product $i",
+                "orientation" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro debitis eius deserunt odio, repudiandae ad repellendus laboriosam nobis sed?",
+                "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem temporibus, pariatur, tempore quia officiis at repudiandae dolore assumenda sunt fugiat alias illo nam minus autem dolor voluptate. Dignissimos eum natus ipsum optio neque numquam, voluptatem autem! Officiis, voluptas. Dolorum atque minima, aliquam facilis minus exercitationem aliquid doloremque vero, error qui consequatur quas tempore aspernatur asperiores cupiditate similique? Eius esse excepturi repellat deleniti, asperiores quas magni! Labore facere dicta expedita natus quisquam eaque, aspernatur minima quas nobis mollitia soluta sed id incidunt consequatur recusandae. Asperiores distinctio cum recusandae, odit earum quod vero similique assumenda? Autem perferendis ipsa accusamus id eaque. Sapiente!",
+                "price" => rand(5000, 30000),
+                "stock" => rand(10, 100),
+                "discount" => 0.05,
+                "image" => env("DEFAULT_IMAGE_PRODUCT"),
+            ]);
+        }
     }
 }
