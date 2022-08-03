b52418cb434efaf64b25840d15d0dbfadded150d
Najib Fikri <najibfikri13@gmail.com>
2022-08-03 10:35:47 +0700

    tiny animation

---
+++ public/js/landing.js
@@ -0,0 +1,7 @@
+$(".masthead").mousemove(function (event) {
+    var mouseX = (event.pageX * -1) / 150;
+    var mouseY = (event.pageY * -1) / 150;
+
+    $(this).css("background-position", mouseX, mouseY);
+    console.log("background-position", mouseX, "%", mouseY, "%");
+});
