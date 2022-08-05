0ea79a89529b91e4b0d3553403e328b025f8bb9d
Najib Fikri <najibfikri13@gmail.com>
2022-08-05 09:22:30 +0700

    raja ongkir api

---
+++ app/Http/Controllers/RajaOngkirController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RajaOngkirController extends Controller
+{
+    function _ongkir_post($origin, $des, $qty, $cour)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "origin=" . $origin . "&destination=" . $des . "&weight=" . $qty . "&courier=" . $cour,
+            CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded",
+                "key: " . env("API_RAJAONGKIR")
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            return $err;
+        } else {
+            return $response;
+        }
+    }
+
+
+    function _ongkir_get($data)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://api.rajaongkir.com/starter/" . $data,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "key: " . env("API_RAJAONGKIR")
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            return  $err;
+        } else {
+            return $response;
+        }
+    }
+
+
+    public function province()
+    {
+        $province = $this->_ongkir_get('province');
+        $data = json_decode($province, true);
+        echo json_encode($data['rajaongkir']['results']);
+    }
+
+    public function city($province = "")
+    {
+        if (!empty($province)) {
+            if (is_numeric($province)) {
+                $city = $this->_ongkir_get('city?province=' . $province);
+                $data = json_decode($city, true);
+                echo json_encode($data['rajaongkir']['results']);
+            } else {
+                abort(404);
+            }
+        } else {
+            abort(404);
+        }
+    }
+
+
+    public function price($origin, $des, $qty, $cour)
+    {
+        $price = $this->_ongkir_post($origin, $des, 300, $cour); // 300 gram/pieces for every product
+        $data = json_decode($price, true);
+        echo json_encode($data['rajaongkir']["results"]);
+    }
+}
